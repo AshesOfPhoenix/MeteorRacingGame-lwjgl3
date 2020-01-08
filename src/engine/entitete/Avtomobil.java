246b452cd9392c4ca1466b2319d4f33205a0e55c
Kristjan Križman <37045415+AshesOfPhoenix@users.noreply.github.com>
2020-01-08 13:44:06 +0100

    Multitexturing

--- src/engine/entitete/Avtomobil.java
+++ src/engine/entitete/Avtomobil.java
@@ -6,20 +6,41 @@ import org.lwjgl.glfw.GLFW;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Avtomobil extends Entity {
-    private static final float noPOWER = 3;
-    private static final float TURN_SPEED = 20;
-    static float accnumber = 2;
-    private static float RUN_SPEED = 50;
+    private static final float RUN_SPEED = 50;
+    private static final float TURN_SPEED = 30;
     boolean speedBoost = false;
     boolean armour = false;
+
+    private float xSize;
+    private float ySize;
+    private float zSize;
 
     private float currentSpeed = 0;
     private float currentTS = 0;
     private Vector3f pozicija;
 
+    public Vector3f getPozicija() {
+        return pozicija;
+    }
+
+    public float getxSize() {
+        return xSize;
+    }
+
+    public float getySize() {
+        return ySize;
+    }
+
+    public float getzSize() {
+        return zSize;
+    }
+
     public Avtomobil(TextureModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
         super(model, position, rotX, rotY, rotZ, scale);
-        this.pozicija = position;
+        this.xSize = model.getRawModel().getxSize() * scale;
+        this.zSize = model.getRawModel().getySize() * scale;
+        this.ySize = model.getRawModel().getzSize() * scale;
+        this.pozicija = new Vector3f(position.x, position.y + ySize / 2, position.z);
     }
 
     public void move() {
@@ -32,45 +53,19 @@ public class Avtomobil extends Entity {
         float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotZ())));
         float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotZ())));
         super.increasePosition(dx, 0, dz);
-
-        if (pozicija.x >= 10000 || pozicija.x < 0 || pozicija.z >= 5000 || pozicija.z < 0) {
-            for (int i = 0; i < 1000000; i++) {
-                super.increasePosition(0, (float) 0.001, 0);
-            }
-        }
-
+        this.pozicija = super.getPosition();
     }
 
     //check the keyboard input and set car moving speed
     private void checkInputs() {
         if (Input.isKeyDown(GLFW.GLFW_KEY_W)) {
-            if (this.currentSpeed > -RUN_SPEED) {
-                this.currentSpeed += -accnumber;
-                System.out.print(currentSpeed);
-            }
+            this.currentSpeed = -RUN_SPEED;
         } else if (Input.isKeyDown(GLFW.GLFW_KEY_S)) {
-            if (this.currentSpeed < RUN_SPEED) {
-                this.currentSpeed += accnumber;
-            } else {
-                this.currentSpeed = RUN_SPEED;
-            }
-        } /*else if (Input.isKeyDown(GLFW.GLFW_KEY_SPACE)) {
-            this.currentSpeed = 0;
-        }*/ else if (this.currentSpeed > 0) {
-            this.currentSpeed += -noPOWER;
-            if (this.currentSpeed < 1) {
-                this.currentSpeed = 0;
-            }
-            System.out.println(currentSpeed);
-        } else if (this.currentSpeed < 0) {
-            this.currentSpeed += noPOWER;
-            if (this.currentSpeed > -1) {
-                this.currentSpeed = 0;
-            }
-            System.out.println(currentSpeed);
-        } else if (this.currentSpeed < 0.9 && this.currentSpeed > 0.1) {
+            this.currentSpeed = RUN_SPEED;
+        } else {
             this.currentSpeed = 0;
         }
+
         if (Input.isKeyDown(GLFW.GLFW_KEY_D)) {
             this.currentTS = -TURN_SPEED;
         } else if (Input.isKeyDown(GLFW.GLFW_KEY_A)) {
@@ -78,8 +73,8 @@ public class Avtomobil extends Entity {
         } else {
             this.currentTS = 0;
         }
-
     }
+
     public void colisiondetection(Meteor meteor) {
         Vector3f pozicija = meteor.getPosition();
         if (pozicija.x - this.pozicija.x <= 50 && pozicija.y - this.pozicija.y <= 50 && pozicija.z - this.pozicija.z <= 50) {
@@ -97,13 +92,9 @@ public class Avtomobil extends Entity {
 
     public void activateSpeedBoost() {
         this.speedBoost = true;
-        this.accnumber = 5;
-        this.RUN_SPEED = 70;
     }
 
     public void disableSpeedBoost() {
         this.speedBoost = false;
-        this.accnumber = 2;
-        this.RUN_SPEED = 50;
     }
 }
---
+++ src/engine/maths/Collision.java
@@ -0,0 +1,50 @@
+package engine.maths;
+
+import engine.entitete.Avtomobil;
+import engine.entitete.Entity;
+import engine.entitete.Meteor;
+import org.lwjgl.util.vector.Vector3f;
+
+public class Collision {
+    Avtomobil car;
+
+    public Collision(Avtomobil car) {
+        this.car = car;
+    }
+
+    public boolean CheckCollisionBox(Entity collidedEntity) {
+
+
+        return false;
+    }
+
+    public boolean CheckCollisionSphere(Meteor collidedEntity) {
+        //?GET METEOR RADIUS
+        float radius = collidedEntity.getRadius();
+        //?GET OBJECTS POSITIONS
+        Vector3f centerSphere = collidedEntity.getPositionA();
+        Vector3f centerCar = car.getPozicija();
+        //?GET HALF-WIDTH AND HALF-HEIGHT OF A AABB (CAR)
+        Vector3f aabb_half_extents = new Vector3f(this.car.getxSize() / 2, this.car.getySize() / 2, this.car.getzSize() / 2);
+        //?DIFFERENCE VECTOR BETWEEN BOTH CENTERS
+        Vector3f difference = new Vector3f(centerSphere.x - centerCar.x, centerSphere.y - centerCar.y, centerSphere.z - centerCar.z);
+        //?Add clamped value to AABB_center and we get the value of box closest to circle
+        Vector3f clamped = clamp(difference, new Vector3f(-1 * aabb_half_extents.x, -1 * aabb_half_extents.y, -1 * aabb_half_extents.z), aabb_half_extents);
+        Vector3f closest = new Vector3f(centerCar.x + clamped.x, centerCar.y + clamped.y, centerCar.z + clamped.z);
+
+        //?Retrieve vector between center circle and closest point AABB and check if length <= radius
+        difference = new Vector3f(closest.x - centerSphere.x, closest.y - centerSphere.y, closest.z - centerSphere.z);
+        return lenghtVec(difference) < radius;
+    }
+
+    public Vector3f clamp(Vector3f value, Vector3f vec1, Vector3f vec2) {
+        float X = (float) Math.max(vec1.x, Math.min(vec2.x, value.x));
+        float Y = (float) Math.max(vec1.y, Math.min(vec2.y, value.y));
+        float Z = (float) Math.max(vec1.z, Math.min(vec2.z, value.z));
+        return new Vector3f(X, Y, Z);
+    }
+
+    public float lenghtVec(Vector3f vector3f) {
+        return (float) Math.sqrt(Math.pow(vector3f.x, 2) + Math.pow(vector3f.y, 2) + Math.pow(vector3f.z, 2));
+    }
+}
